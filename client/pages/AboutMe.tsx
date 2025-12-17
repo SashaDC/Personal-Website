209a543dbf18ccd6aafeacb956b821667bea80e3
Sasha Chambers <sashachambers10.12@gmail.com>
2025-12-17 20:40:00 +1300

    Fixed routes and Added extra page.

---
+++ client/pages/AboutMe.tsx
@@ -0,0 +1,8 @@
+export default function AboutMe() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold mb-4">About Me</h1>
+      <p className="text-lg max-w-xl text-center">example.</p>
+    </div>
+  )
+}
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,13 +1,16 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import AboutMe from './pages/AboutMe'
 import Projects from './pages/Projects'
 import Blogs from './pages/Blogs'
 import Contact from './pages/Contact'
 
 export default createRoutesFromElements(
-  <Route path="/" element={<Home />}>
+  <>
+    <Route path="/" element={<Home />} />
+    <Route path="/about-me" element={<AboutMe />} />
     <Route path="/projects" element={<Projects />} />
     <Route path="/blogs" element={<Blogs />} />
     <Route path="/contact" element={<Contact />} />
-  </Route>
+  </>
 )
