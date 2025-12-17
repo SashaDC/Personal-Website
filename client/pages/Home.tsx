32107399a7756692397317f0d9df6119479daa91
Sasha Chambers <sashachambers10.12@gmail.com>
2025-12-17 20:40:50 +1300

    Added a bit of styling and functionality to the home page.

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,11 +1,29 @@
+import Navbar from '../components/Navbar'
+
 export default function Home() {
+  // Home page content
+  // Needs: name, Title, navbar, last updated date
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white">
-      <h1 className="bg-blue-500">Welcome to My Personal Website!</h1>
-      <p className="text-lg max-w-xl text-center">
-        This is the home page. You can add a hero section, intro, or
-        call-to-action here.
-      </p>
+    <div
+      className="min-h-screen flex flex-col items-center justify-center bg-[url(public/test.png)]"
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('public/test.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <header className="text-center space-y-3">
+        <h1 className="text-6xl font-bold drop-shadow-[0_4px_8px_rgba(0,0,0,1)]">
+          Sasha Chambers
+        </h1>
+        <h2 className="text-3xl font-semibold drop-shadow-[0_4px_8px_rgba(0,0,0,1)]">
+          Software Developer
+        </h2>
+      </header>
+      <Navbar />
+      <footer className="w-full text-center fixed bottom-2 text-stone-300 drop-shadow-[0_4px_8px_rgba(0,0,0,1)]">
+        <p>(Last Updated: {new Date().toLocaleDateString()})</p>
+      </footer>
     </div>
   )
 }
