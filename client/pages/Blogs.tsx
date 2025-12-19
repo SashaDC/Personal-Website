d62de2a16bc8c05c00bd6b8cf0d15605321242f3
Sasha Chambers <sashachambers10.12@gmail.com>
2025-12-19 23:20:19 +1300

    formatted similar pages to use repeatable components

--- client/pages/Blogs.tsx
+++ client/pages/Blogs.tsx
@@ -1,11 +1,17 @@
+import PostsHeader from '../components/Headers'
+import PostsDisplay from '../components/PostsDisplay'
+import { blogs } from '../../posts/Blogs'
+import { ToTheTop } from '../components/Buttons'
+
 export default function Blogs() {
   // Blogs page content
   // Needs: list of blog posts, which will mainly be the coverage of my journey at dev academy
   // and display the reflections from my previous site
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold mb-4">Blogs</h1>
-      <p className="text-lg max-w-xl text-center">example.</p>
+    <div className="min-h-screen flex flex-col items-center justify-center mt-10 px-4">
+      <PostsHeader Text="Blogs" />
+      <PostsDisplay posts={blogs} />
+      <ToTheTop />
     </div>
   )
 }
--- client/pages/Projects.tsx
+++ client/pages/Projects.tsx
@@ -1,11 +1,17 @@
+import PostsHeader from '../components/Headers'
+import PostsDisplay from '../components/PostsDisplay'
+import { projects } from '../../posts/Projects'
+import { ToTheTop } from '../components/Buttons'
+
 export default function Projects() {
   // Projects page content
   // Needs: list of projects with descriptions and links to live demos or repositories
   // will be in a rounded card style layout listed by most recent
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold mb-4">Projects</h1>
-      <p className="text-lg max-w-xl text-center">example.</p>
+    <div className="min-h-screen flex flex-col items-center justify-center mt-10 px-4 ">
+      <PostsHeader Text="Projects" />
+      <PostsDisplay posts={projects} />
+      <ToTheTop />
     </div>
   )
 }
