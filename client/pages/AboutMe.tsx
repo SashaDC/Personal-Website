0b20058ae036486f15cb3c7201edbfa4fa6a077b
Sasha Chambers <sashachambers10.12@gmail.com>
2025-12-19 23:21:01 +1300

    Pre-added the backtotop button

--- client/pages/AboutMe.tsx
+++ client/pages/AboutMe.tsx
@@ -1,8 +1,12 @@
+import { ToTheTop } from '../components/Buttons'
+
 export default function AboutMe() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-6xl font-bold mb-4">About Me</h1>
-      <p className="text-lg max-w-xl text-center">example.</p>
+      <main>
+        <ToTheTop />
+      </main>
     </div>
   )
 }
