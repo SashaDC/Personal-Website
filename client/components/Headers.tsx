e9a4723b031a80077b1ddacff97763569bb9901e
Sasha Chambers <sashachambers10.12@gmail.com>
2025-12-21 12:31:01 +1300

    Matching styles

--- client/components/Headers.tsx
+++ client/components/Headers.tsx
@@ -2,14 +2,14 @@ import { Back } from './Buttons'
 
 export default function PostsHeader({ Text }: { Text: string }) {
   return (
-    <header>
+    <header className="text-center mb-8 mt-10">
       <h1 className="text-6xl font-bold mb-4">{Text}</h1>
-      <p>
+      <p className="text-lg text-gray-300">
         Check out my {Text.toLowerCase()} below. I don't have a lot of content
         yet, but I'll be updating this page as I continue my journey as a
         developer. (Oldest at the bottom)
       </p>
-      <nav>
+      <nav className="mt-4">
         <Back />
       </nav>
     </header>
