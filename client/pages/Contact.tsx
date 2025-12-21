26ebeaca8b326f869b4f1b277abf50662144f1d9
Sasha Chambers <sashachambers10.12@gmail.com>
2025-12-22 10:48:42 +1300

    Hopefully fixed ios scroll issue

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -2,7 +2,7 @@ import { Back } from '../components/Buttons'
 
 export default function Contact() {
   return (
-    <div className="min-h-screen max-h-screen overflow-hidden flex flex-col items-center justify-center px-4">
+    <div className="min-h-screen flex flex-col items-center justify-center px-4 py-8 md:max-h-screen md:overflow-hidden">
       <header className="text-center mb-8">
         <h1 className="text-6xl font-bold mb-4">Contact Me</h1>
         <p className="text-lg text-gray-300 max-w-2xl">
